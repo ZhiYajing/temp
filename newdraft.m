2b4f5efbbc83abeb37cf2236d98c56d7514ab49b
ZhiYajing <chinesezhiyajing@163.com>
2018-05-31 16:52:06 +0800

    Update newdraft.m

--- newdraft.m
+++ newdraft.m
@@ -1,17 +1,30 @@
 function x=newdraft(L,S1,S2,b)
 [n,n]=size(L);[m1,m1]=size(S1);[m2,m2]=size(S2);m=m1*m2;
-% n=32;m1=8;m2=4;m=m1*m2;
+% n=4;m1=2;m2=4;m=m1*m2;
 % a=10*(rand(n,1));L=tril(toeplitz(a));
-% S=toeplitz([2 -1 zeros(1, m-2)]);
+% S1=toeplitz([2 -1 zeros(1, m1-2)]);
+% S2=toeplitz([2 -1 zeros(1, m2-2)]);
 % b=10*(rand(n*m,1));
 % solve: S=(kron(eye(m1),S2) + kron(S1,eye(m2)));
-% (kron(L,eye(m)) + kron(eye(n),S)) x = b
+% (kron(L,eye(m)) + kron(eye(n),(kron(eye(m1),S2) + kron(S1,eye(m2))))) x = b
+% xx=inv(kron(L,eye(m)) + kron(eye(n),(kron(eye(m1),S2) + kron(S1,eye(m2)))))*b
 
 % S1= Q1'*A1*Q1, A1=diag(eig(S1));
 % S2= Q2'*A2*Q2, A2=diag(eig(S2));
 % S_hat=(kron(Q1',Q2'))*(kron(eye(m1),A2)+kron(A1,eye(m2)))*(kron(Q1,Q2))
 %      = Q_hat'*A_hat*Q_hat;
-a1=eig(S1);a2=eig(S2);
+
+%a1=eig(S1);a2=eig(S2);
+for i=1:m1
+    a1(m1-i+1)=2+2*cos(i*pi/(m1+1));
+end
+for i=1:m2
+    a2(m2-i+1)=2+2*cos(i*pi/(m2+1));
+end
+for i = 1:m1
+   A(:,i)= a2+a1(i);
+end
+a=A(:);
 % A1=diag(a1); A2=diag(a2);
 
 
@@ -36,10 +49,10 @@ for i=1:n
       G(:,k)= sine_transform_data(F(:,k));
    end
    H=G';
-   B(:,i)=H(:);
+   BB(:,i)=H(:);
 end
 
-c=B(:); %c=b_tilde;
+c=BB(:); %c=b_tilde;
 %QB=[Qb1 Qb2 ... Qbn];
 %Qb(i)=fst(b(i));
 %c=QB(:);
@@ -63,21 +76,36 @@ for i=1:m
     Z(:,i)= ToeplitzMatVec(T,y);
     %Z(:,i)=LowToeplitzInv(L+ lamda*eye(n)) * f((i-1)*n+1 : i*n);
 end 
-z=Z(:);
+z=Z(:);%z=x_hat
 
-%z=P'y; y=x_tilde;y=kron(eye(n),Q)x; 
-%x=kron(eye(n),Q')Pz;
+%z=P'y; y=x_tilde;y=kron(eye(n),Q_hat)x; 
+%x=kron(eye(n),Q_hat')Pz;
 %y=Pz=vec(Z');
 Z=Z';
 y=Z(:);
 
-%x=kron(eye(n),Q')y=vec(Q'YI)=vec(Q'Y);
+% x=kron(eye(n),Q_hat')y=vec(Q_hat'YI)=vec(Q_hat'Y);Y=reshape(y,[m,n]);
+% Q_hat'Y=kron(Q1',Q2')*[y1 y2 ... yn]; yi=Y(:,i);
+% kron(Q1',Q2')*yi=Q2'*Y1*Q1; Y1=reshape(yi,[m2,m1]);
+% E=Q2'*Y1=[idst(yi1) idst(yi2) ... idst(yim1)];
+% E*Q1=(Q1'*E')'=(Q1'*F)'=[idst(f1) idst(f2) ... idst(fm2)]';F=E'=[f1 f2 fm2];
+% E*Q1'=(Q1*E')'=(Q1*F)'=[dst(f1) dst(f2) ... dst(fm2)]';F=E'=[f1 f2 fm2];
 Y=reshape(y,[m,n]);
 for i=1:n
-   %Y(:,i)=y((i-1)*m+1 : i*m); 
-   Y(:,i)=sine_transform_data(Y(:,i));
+  Y1=reshape(Y(:,i),[m2,m1]); %Y1=reshape(yi,[m2,m1]);
+  %E=Q2'*Y1
+  for j=1:m1
+     E(:,j)= sine_transform_data(Y1(:,j));
+  end
+  %H=E*Q1=(Q1'*E')'
+  F=E';
+  for k=1:m2
+     G(:,k)= sine_transform_data(F(:,k));
+  end
+  H=G';
+  YY(:,i)=H(:);
 end
-x=Y(:);
+x=YY(:);
 %Q'Y=[Q'y1 Q'y2 ... Q'yn];
 %Q'y(i)=fst(y(i));
 %x=Q'Y(:);
