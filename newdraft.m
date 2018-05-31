b9be4334845f818f6e5070c5007d022114dbbdf0
ZhiYajing <chinesezhiyajing@163.com>
2018-05-31 17:59:34 +0800

    Update newdraft.m

--- newdraft.m
+++ newdraft.m
@@ -112,41 +112,48 @@ x=YY(:);
 end
 
 function X= LowToeplitzInv(A)
-% a=10*(rand(8,1));A=toeplitz(a);A=tril(A);
+% a=10*(rand(9,1));A=toeplitz(a);A=tril(A);
 
 [n,n]= size (A ); X= zeros (n,n);
-    if dividable (A)==1
-        [B ,C]= divide (A );
+     %if dividable (A)==1
+      
+     if n>=2
+         m=pow2(ceil(log2(n)));p=n;
+        if m~= n
+            a=[A(:,1);zeros(m-n,1)];
+            A=toeplitz(a);A=tril(A);
+            n=m;
+        end
+        [B ,C]= divide (A);
         T=LowToeplitzInv (B);
-         
 %         X (1: n /2 ,1: n /2)= T;
 %         X (1: n/2 ,n /2+1: n )= 0;
 %         X(n /2+1: n ,1: n /2)= -1* T* C* T;
 %         X(n /2+1: n ,n /2+1: n )= T;
         
         x(1: n /2)=T*eye(length(T),1);
+        %x(1: n /2)=T(:,1);
 %         x(n /2+1: n)= -T* (C* (T*eye(length(T),1)));
         w=ToeplitzMatVec(C,T(:,1));
         w=ToeplitzMatVec(-T,w);
         x(n /2+1: n)=w;
         
         X (1: n /2 ,1: n /2)= T;
-        X (1: n/2 ,n /2+1: n )= 0;
-        X(n /2+1: n ,1: n /2)= toeplitz(x(n/2+1 : n),x(n/2+1:-1:2));
-        X(n /2+1: n ,n /2+1: n )= T;
+        X (1: n/2 ,n /2+1: p )= 0;
+        %X(n /2+1: n ,1: n /2)= toeplitz(x(n/2+1 : n),x(n/2+1:-1:2));
+        Y=toeplitz(x(n/2+1 : n),x(n/2+1:-1:2));
+        X(n /2+1: p ,1: n /2)= Y(1:(p-n/2),:);
+        %X(n /2+1: n ,n /2+1: n )= T;
+        X(n /2+1: p ,n /2+1: p )= T(1:(p-n/2),1:(p-n/2));
     else X=1/A;
     end
 end
-function X= dividable (A)
-[n,n ]= size (A);
-X =((n >=2)&&( mod (n ,2)==0));
-end
+
 function [B ,C] = divide (A)
 [n ,n ]= size (A );
 
 B=A(1:n /2 ,1: n /2);
 C=A(n /2+1: n ,1: n /2 );
-
 end
 
 function x=ToeplitzMatVec(T,y)
